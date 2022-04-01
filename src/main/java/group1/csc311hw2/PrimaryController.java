aee806cd508914201c47541c46e1d56333237a0a
nka2001 <nandrle01@gmail.com>
2022-04-01 10:04:23 -0400

    CSC 311 HW2

--- src/main/java/group1/csc311hw2/PrimaryController.java
+++ src/main/java/group1/csc311hw2/PrimaryController.java
@@ -2,52 +2,79 @@ package group1.csc311hw2;
 
 import java.io.IOException;
 import javafx.fxml.FXML;
+import javafx.scene.Node;
+import javafx.scene.input.MouseEvent;
+import javafx.scene.layout.GridPane;
 import javafx.scene.layout.Pane;
+import javafx.scene.paint.Color;
 import javafx.scene.shape.Circle;
 import javafx.scene.shape.Rectangle;
 
 public class PrimaryController {
 
+    private int counter = 0;
+    
     @FXML
-    private Pane Pane;
+    private GridPane gameBoard;
+    
     @FXML
-    private Circle cCell1;
-    @FXML
-    private Circle cCell7;
-    @FXML
-    private Circle cCell4;
-    @FXML
-    private Circle cCell8;
-    @FXML
-    private Circle cCell5;
-    @FXML
-    private Circle cCell2;
-    @FXML
-    private Circle cCell9;
-    @FXML
-    private Circle cCell6;
-    @FXML
-    private Circle cCell3;
-    @FXML
-    private Rectangle sCell1;
-    @FXML
-    private Rectangle sCell7;
-    @FXML
-    private Rectangle sCell4;
-    @FXML
-    private Rectangle sCell8;
-    @FXML
-    private Rectangle sCell5;
-    @FXML
-    private Rectangle sCell2;
-    @FXML
-    private Rectangle sCell9;
-    @FXML
-    private Rectangle sCell6;
-    @FXML
-    private Rectangle sCell3;
-
+    public Circle circ = new Circle();
+    public Circle circ2 = new Circle();
+    
+    
+    private Rectangle rect = new Rectangle();
+    private Rectangle rect2 = new Rectangle();
    
+    public void initialize(){
+       
+        circ.setFill(Color.RED);
+        circ.setRadius(10);
+        circ.setOpacity(0);
+        circ2.setFill(Color.RED);
+        circ2.setRadius(10);
+        rect.setFill(Color.BLUE);
+        
     
+        
+        for(int i =0; i< gameBoard.getColumnCount(); i++){
+            for(int j = 0; j < gameBoard.getRowCount(); j++){
+                if(i == 0 && j == 0)
+                    gameBoard.add(circ, 0,0);
+                if( i == 1 && j == 1)
+                    gameBoard.add(circ2, 1,1);
+            }
+        }
+        
+        
+    }
+
+    @FXML
+    private void handleOnMouseClicked(javafx.scene.input.MouseEvent event) {
+        
+        mouseEntered(event);
+        while(counter <= 10){
+            if(counter % 2 == 1 && rect.getOpacity() != 1 ){//try getting the cell clicked on, or just seeing if the cell is cell 0,0 for these if elses
+                circ.setOpacity(1);
+                
+            
+            }
+            if(counter % 2 == 0 && circ.getOpacity() != 1){
+                rect.setOpacity(1);
+                
+                
+            }
+            
+            counter++;
+        }
+        
+        
+    }
     
+    private void mouseEntered(MouseEvent e){
+        Node s = e.getPickResult().getIntersectedNode();
+        Integer col = GridPane.getColumnIndex((Node) e.getTarget());
+        Integer row = GridPane.getRowIndex(s);
+        System.out.println(col + " " + row);
+    }
+
 }
